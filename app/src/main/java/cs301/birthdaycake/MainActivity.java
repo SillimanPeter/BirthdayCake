b3f2d904c98ea6ea9b20786f2461c7bb89fe1536
Peter Silliman <petersilliman03@gmail.com>
2022-02-06 16:03:15 -0800

    Events Lab 3

--- app/src/main/java/cs301/birthdaycake/MainActivity.java
+++ app/src/main/java/cs301/birthdaycake/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +14,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
+
+        CakeView cakeView = findViewById(R.id.cakeview);
+        CakeController cakeController = new CakeController(cakeView);
+
+        Button blowOut = findViewById(R.id.blowOut);
+        blowOut.setOnClickListener(cakeController);
     }
 
     public void goodbye(View button){
